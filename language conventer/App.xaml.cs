1d666156de05c4695283c4ecfece9fc404b61013
Joshua Shields <shields.joshua23@gmail.com>
2024-07-04 20:36:30 +0200

    Add project files.

---
+++ language conventer/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace language_conventer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
